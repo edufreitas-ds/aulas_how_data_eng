89a27a427120dc8ae607939c52dda3650c81b4c1
edufreitas-ds <edufreitasouza@gmail.com>
2023-03-18 16:41:03 -0300

    [AULA 03][ADD] Backoff lib + error handling

--- aulas_how/aula_03/main.py
+++ aulas_how/aula_03/main.py
@@ -3,18 +3,145 @@ import requests
 import json
 
 #%%
+
+# APIs
 url = 'https://economia.awesomeapi.com.br/json/last/USD-BRL'
 ret = requests.get(url)
 
 #%%
+
+# API requisition status code
+    # 1XX - info
+    # 2XX - sucess (this is what we want!)
+    # 3XX - redirect
+    # 4xx - client error (you made a mistake)
+    # 5xx - server error (they made a mistake)
 if ret:
-  print(ret)
+	print(ret)
 else:
-  print("Failed.")
+	print("Failed.")
 
 # %%
 dolar = json.loads(ret.text)['USDBRL']
 # %%
 print(f"20 dólares hoje custam R$ {float(dolar['bid']) * 20}.")
+
+# %%
+def cotacao(valor, moeda):
+	url = "https://economia.awesomeapi.com.br/json/last/{}".format(moeda)
+	ret = requests.get(url)
+	dolar = json.loads(ret.text)[moeda.replace('-','')]
+	cotacao = f"{valor} {moeda[:3]} hoje custam {float(dolar['bid']) * 20} {moeda[-3:]}."
+	print(cotacao)
+# %%
+cotacao(20, "USD-BRL")
+# %%
+cotacao(20, "JPY-BRL")
+# %%
+cotacao(20, "EDUARDO")
+# %%
+
+
+try:
+	cotacao(20, "EDUARDO")
+except:
+	pass
+# %%
+
+
+try:
+	cotacao(20, "JPY-BRL")
+except:
+	pass
+else:
+	print("Cotação funcionou.")
+# %%
+
+
+try:
+	cotacao(20, "EDUARDO")
+except Exception as e:
+	print(f"Erro no parâmetro: {e}")
+else:
+	print("Cotação funcionou.")
+# %%
+
+
+try:
+	10/0
+except Exception as e:
+	print(e)
+else:
+	print("ok")
+
+
+# %%
+
+def error_check(func):
+	def inner_func(*args, **kargs):
+		try:
+			func(*args, **kargs)
+		except:
+			print(f"Função {func.__name__} falhou.")
+	return inner_func
+
+			
+@error_check			
+def multi_moedas(valor, moeda):
+		url = "https://economia.awesomeapi.com.br/json/last/{}".format(moeda)
+		ret = requests.get(url)
+		dolar = json.loads(ret.text)[moeda.replace('-','')]
+		cotacao = f"{valor} {moeda[:3]} hoje custam {round(float(dolar['bid']) * 20, 2)} {moeda[-3:]}."
+		print(cotacao)
+			
+# %%
+
+
+multi_moedas(valor=20, moeda="USD-BRL")
+multi_moedas(valor=20, moeda="EUR-BRL")
+multi_moedas(valor=20, moeda="BTC-BRL")
+multi_moedas(valor=20, moeda="JPY-BRL")
+multi_moedas(valor=20, moeda="RPL-BRL")
+# %%
+
+
+import backoff
+import random
+
+# Exposing errors and applying retries
+@backoff.on_exception(backoff.expo, (ConnectionAbortedError, ConnectionRefusedError, TimeoutError),
+                    max_tries=10)
+def test_func(*args, **kargs):
+    rnd = random.random()
+    print(f"""
+            RND: {rnd}
+            args: {args if args else 'no args'}
+            kargs: {kargs if kargs else 'no kargs'}
+    """)
+    if rnd < .2:
+        raise ConnectionAbortedError("Conexão abortada.")
+    elif rnd < .4:
+        raise ConnectionRefusedError("Conexão recusada.")
+    elif rnd < .6:
+        raise TimeoutError("Tempo de espera excedido.")
+    else:
+        return "OK! Número randômico (RND) aceito."
+# %%
+
+# no args / no kargs
+test_func()
+
+# %%
+
+# args / no kargs
+test_func(1)
+# %%
+
+# no args / kargs
+test_func(name = "EDUARDO")
+# %%
+
+# args / kargs
+test_func(1, name = "EDUARDO")
 
 # %%
